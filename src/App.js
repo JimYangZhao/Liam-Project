1d4242428245b1a3015bca16096b5dc770b35181
ShadowZY's Home <yangshome@ShadowZYsdeMacBook-Air.local>
2021-01-07 14:27:48 -0330

    first push Thursday

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+
+function App() {
+  return (
+
+    //BEM
+    <div className="app">
+      <Header />
+      {/* <Navigation /> */}
+      <Home />
+    
+
+    {/* Header */}
+
+        {/* Banner */}
+
+        {/* Cards */}
+
+        {/* Footer */}
+
+    {/* SearchPage */}
+        {/* .. */}
+    
+    </div>
+  );
+}
+
+export default App;
